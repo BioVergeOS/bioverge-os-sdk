d816ef02acd896df0b04fcb68969016fcd297605
BioVergeOS <biovergeoserc@gmail.com>
2025-06-27 22:30:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="bioverge-os-sdk",
     version="0.1.0",
     description="SDK for BioVerge OS simulation and NFT platform",
-    author="Your Name or Organization",
+    author="BioVerge OS",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.8",
