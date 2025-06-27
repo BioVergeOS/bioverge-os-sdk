c5c6e456847b952009a60af735eacb50dd4293e0
BioVergeOS <biovergeoserc@gmail.com>
2025-06-27 22:29:35 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bioverge-os-sdk",
+    version="0.1.0",
+    description="SDK for BioVerge OS simulation and NFT platform",
+    author="Your Name or Organization",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.8",
+)
